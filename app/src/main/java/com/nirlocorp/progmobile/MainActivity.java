8549deeffbf4702cb12acecb1b164c3b26cfcd1b
GuillaumeNirlo <guillaume.nirlo@hotmail.fr>
2019-04-04 16:26:44 +0400

    first commit

---
+++ app/src/main/java/com/nirlocorp/progmobile/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nirlocorp.progmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
